07927e591d691f204d3699848aba81df14087612
Rillus <me@ariley.me>
2013-07-29 14:46:07 +0100

    initial commit with fresh ding-admin installation

---
+++ README.md
@@ -0,0 +1,4 @@
+folios
+======
+
+Portfolio creator built on the ding-admin system
---
+++ application/views/templates/footer.php
@@ -0,0 +1,8 @@
+	</div>
+</div>
+<?php if (!isset($scripts)){
+	$scripts = "";
+}
+$this->load->view('templates/javascript', $scripts); ?>
+</body>
+</html>
